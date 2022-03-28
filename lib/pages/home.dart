ca051084b0405f812e50ebc23bca503dc5b84891
Hayder Ali <hyderali0889@gmail.com>
2022-03-28 22:19:42 +0500

    added sign in with email

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,28 +12,108 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   FirebaseAuth auth = FirebaseAuth.instance;
 
+  var email;
+  var password;
+  var email2;
+  var password2;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
 
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user == null) {
         print('User is currently signed out!');
       } else {
-        print('User is signed in! ${user.displayName}');
+        print('User is signed in! ${user.email}');
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Center(
+    return Scaffold(
+        body: Container(
+      padding:
+          EdgeInsets.only(top: MediaQuery.of(context).viewPadding.top + 50),
+      child: Column(
+        children: [
+          Center(
+            child: Column(children: [
+              TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Email",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                        borderSide: const BorderSide(color: Colors.red))),
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14.0),
+                          borderSide: const BorderSide(color: Colors.red))),
+                  onChanged: (value) {
+                    setState(() {
+                      password = value;
+                    });
+                  }),
+              TextButton(
+                  onPressed: signupwithemail, child: const Text("Sign Up"))
+            ]),
+          ),
+          Center(
             child: TextButton(
-      child: const Text("Sign In With Google"),
-      onPressed: signInWithGoogle,
-    )));
+              child: const Text("sign Out"),
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+              },
+            ),
+          ),
+          Center(
+            child: Column(children: [
+              TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Email",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                        borderSide: const BorderSide(color: Colors.red))),
+                onChanged: (value) {
+                  setState(() {
+                    email2 = value;
+                  });
+                },
+              ),
+              TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter password",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14.0),
+                          borderSide: const BorderSide(color: Colors.red))),
+                  onChanged: (value) {
+                    setState(() {
+                      password2 = value;
+                    });
+                  }),
+              TextButton(
+                  onPressed: signinwithemail, child: const Text("Sign In"))
+            ]),
+          ),
+          Center(
+              child: TextButton(
+            child: const Text("Sign In With Google"),
+            onPressed: signInWithGoogle,
+          )),
+        ],
+      ),
+    ));
   }
 
   Future<UserCredential> signInWithGoogle() async {
@@ -52,5 +132,33 @@ class _HomepageState extends State<Homepage> {
 
     // Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  void signupwithemail() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void signinwithemail() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email2, password: password2);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
   }
 }
